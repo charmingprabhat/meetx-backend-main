f2d46183a59d33f79e58fc3f9c3986c83e1f0df2
Prabhat Kumar <prabhatcse23@gmail.com>
2026-05-25 00:55:26 +0530

    Fixed CORS issue

--- src/main/java/com/videomeeting/backend/controller/UsersController.java
+++ src/main/java/com/videomeeting/backend/controller/UsersController.java
@@ -5,33 +5,27 @@ import com.videomeeting.backend.requests.LoginRequest;
 import com.videomeeting.backend.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "https://meetx-frontend-alpha.vercel.app")
+@CrossOrigin(origins = {
+        "https://meetx-frontend-alpha.vercel.app",
+        "https://meetx-frontend-git-main-charmingprabhats-projects.vercel.app"
+})
 public class UsersController {
 
     @Autowired
     UserService userService;
 
     @PostMapping("/addUser")
-    public String addUser(
-            @RequestBody Users user
-    ) {
+    public String addUser(@RequestBody Users user) {
 
-        return userService.addUser(
-                user
-        );
+        return userService.addUser(user);
     }
 
     @PostMapping("/loginUser")
-    public String loginUser(
-            @RequestBody LoginRequest request
-    ) {
+    public String loginUser(@RequestBody LoginRequest request) {
 
-        return userService.loginUser(
-                request
-        );
+        return userService.loginUser(request);
     }
 }
